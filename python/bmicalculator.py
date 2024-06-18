566a3437a7631babad804a39b688446092667501
Swasthik11 <saiswasthikdshetty124@gmail.com>
2024-06-18 11:37:23 +0530

    bmi comi

---
+++ python/bmicalculator.py
@@ -0,0 +1,8 @@
+print("enter the height of the person(in meters)")
+height=input()
+print("enter the weight of the person(in kgs)")
+weight=input()
+h=float(height)
+w=float(weight)
+bmi=w/h ** 2
+print(bmi)
